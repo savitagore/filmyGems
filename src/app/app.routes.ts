537ebecd85a22d34204064b292576117686a173e
Savitagore <savitag096@gmail.com>
2024-10-02 13:32:00 +0530

    creates footers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // { path: 'footer', component: FooterComponent },
+
+];
