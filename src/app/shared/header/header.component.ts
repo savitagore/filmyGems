8ee52ae2fd0b0e317ff30dc942e3f796ef14bbb5
Savitagore <savitag096@gmail.com>
2024-10-14 09:54:39 +0530

    changed

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent],
+  imports: [RouterOutlet,FooterComponent,RouterLink ,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  backgroundImageUrl: string = '/assets/landing_Img/landing-bg.png';
 }
