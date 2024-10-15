924b46e37f485d9c73c001f180d840389fd1c781
Savitagore <savitag096@gmail.com>
2024-10-15 10:10:49 +0530

    complete css style

--- src/app/pages/create-profile/create-profile.component.ts
+++ src/app/pages/create-profile/create-profile.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,NavbarComponent,RouterLink],
   templateUrl: './create-profile.component.html',
   styleUrl: './create-profile.component.css'
 })
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -4,11 +4,12 @@ import { FooterComponent } from '../footer/footer.component';
 import Aos from 'aos';
 import { CommonModule } from '@angular/common';
 import { BlogsComponent } from '../blogs/blogs.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,RouterLink ,CommonModule,BlogsComponent],
+  imports: [RouterOutlet,FooterComponent,RouterLink ,CommonModule,BlogsComponent,NavbarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
