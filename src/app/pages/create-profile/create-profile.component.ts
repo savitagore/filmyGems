d8ccd09d60a1da61508bd9271fe418cee53bc664
Savitagore <savitag096@gmail.com>
2024-10-16 09:22:49 +0530

    changed

--- src/app/pages/create-profile/create-profile.component.ts
+++ src/app/pages/create-profile/create-profile.component.ts
@@ -4,19 +4,15 @@ import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { RouterLink } from '@angular/router';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 
 
 @Component({
   selector: 'app-create-profile',
   standalone: true,
-  imports: [CommonModule,NavbarComponent,FooterComponent, MatFormFieldModule,
-    MatInputModule,  ReactiveFormsModule,
-    MatButtonModule,],
+  imports: [CommonModule,NavbarComponent,FooterComponent,
+    ],
     providers:[],
   templateUrl: './create-profile.component.html',
   styleUrl: './create-profile.component.css'
@@ -24,11 +20,5 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 export class CreateProfileComponent {
   backgroundImageUrl: string = 'assets/create-profile/bg-main.png';
 
-  email = new FormControl('', [Validators.required, Validators.email]);
 
-  getErrorMessage() {
-    return this.email.hasError('required') ? 'You must enter a value' :
-        this.email.hasError('email') ? 'Not a valid email' :
-            '';
-  }
 }
