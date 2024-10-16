b566144844624bd77e9191c0995ce8594c1b9659
kaverikamble1805 <kaverikamble1805@gmail.com>
2024-10-16 11:43:19 +0530

    font-wight

--- src/app/pages/plans/plans.component.ts
+++ src/app/pages/plans/plans.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-plans',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,NavbarComponent],
   templateUrl: './plans.component.html',
   styleUrl: './plans.component.css'
 })
