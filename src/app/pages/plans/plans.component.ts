f62111e3586be8817b1e6b271850bc51e1389e1a
kaverikamble1805 <kaverikamble1805@gmail.com>
2024-10-16 14:56:18 +0530

    plans-card

--- src/app/pages/plans/plans.component.ts
+++ src/app/pages/plans/plans.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-plans',
   standalone: true,
-  imports: [CommonModule,NavbarComponent],
+  imports: [CommonModule,NavbarComponent,FooterComponent],
   templateUrl: './plans.component.html',
   styleUrl: './plans.component.css'
 })
