66667e1bcb4ba5cff8006a078714a9c83dd430ea
Savitagore <savitag096@gmail.com>
2024-10-17 09:42:31 +0530

    create profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from './shared/header/header.component';
 import { PlansComponent } from './pages/plans/plans.component';
 import { CreateProfileComponent } from './pages/create-profile/create-profile.component';
 
+
 export const routes: Routes = [
   { path: '', component: HeaderComponent },
   {
