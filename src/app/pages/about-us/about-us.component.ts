c6a2a562f65b87e603752da37d54f62601ab0caf
Priya-kamble-154 <priyakamble2109@gmail.com>
2024-10-21 17:02:01 +0530

    responsive in progress

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
+import { StartjourneyComponent } from '../../shared/startjourney/startjourney.component';
 
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [CommonModule,NavbarComponent],
+  imports: [CommonModule,NavbarComponent,FooterComponent,StartjourneyComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
 export class AboutUsComponent {
 
   backgroundImageUrl: string = 'assets/aboutUs/about-Bg.svg';
+  bgImage: string = 'assets/aboutUs/musicalGirl.png';
 }
