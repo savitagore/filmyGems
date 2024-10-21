5583c46150831bd90d0d71e4063610598046e42f
kaverikamble1805 <kaverikamble1805@gmail.com>
2024-10-21 11:48:58 +0530

    privacy-page

--- src/app/footer/pages/privacypolicy/privacypolicy.component.ts
+++ src/app/footer/pages/privacypolicy/privacypolicy.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-privacypolicy',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './privacypolicy.component.html',
   styleUrl: './privacypolicy.component.css'
 })
