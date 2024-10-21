a147dbc57a08a3dc5879c27ae7f62c5b93e13906
Priya-kamble-154 <priyakamble2109@gmail.com>
2024-10-21 10:23:23 +0530

    aboutUs UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { PlansComponent } from './pages/plans/plans.component';
 import { CreateProfileComponent } from './pages/create-profile/create-profile.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 export const routes: Routes = [
   { path: '', component: HeaderComponent },
@@ -11,6 +12,10 @@ export const routes: Routes = [
   },{
     path:'profile',
     component:CreateProfileComponent
+  },
+  {
+    path:'about',
+    component:AboutUsComponent
   }
 
 ];
