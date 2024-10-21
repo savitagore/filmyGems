6eb93d062def59a8310fea754008c1861d13a17a
Savitagore <savitag096@gmail.com>
2024-10-21 15:00:54 +0530

    add policy

--- src/app/footer/pages/privacypolicy/privacypolicy.component.ts
+++ src/app/footer/pages/privacypolicy/privacypolicy.component.ts
@@ -1,14 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { FooterComponent } from '../../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-privacypolicy',
   standalone: true,
-  imports: [RouterLink,CommonModule,RouterLinkActive],
+  imports: [RouterLink,CommonModule,RouterLinkActive,FooterComponent],
   templateUrl: './privacypolicy.component.html',
   styleUrl: './privacypolicy.component.css'
 })
 export class PrivacypolicyComponent {
+  isTermsActive: boolean = false; // Initialize to false to hide by default
 
+
+  toggleSection(section: string) {
+    if (section === 'terms') {
+      this.isTermsActive = true;  // Show terms, hide privacy
+    } else if (section === 'privacy') {
+      this.isTermsActive = false; // Show privacy, hide terms
+    }
+  }
 }
