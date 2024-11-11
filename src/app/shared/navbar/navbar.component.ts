2d52b5a06b97543472cc61cb5186146daadc5e3a
Savitagore <savitag096@gmail.com>
2024-11-11 19:43:35 +0530

    profile-model-pop up

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,14 +1,43 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive,CommonModule],
+  imports: [RouterLink,RouterLinkActive,CommonModule,ReactiveFormsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isEditMode = false;
+  profileForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.profileForm = this.fb.group({
+      fullName: ['Ajay Kumar'],
+      subscriptionPlan: ['Platinum (₹1999)'],
+      location: ['Navi Mumbai'],
+      contactNumber: ['9800000000'],
+      bio: [
+     'I am Passionate and dedicated, brings complex characters to life on stage and screen. With a rich background in dramatic arts and a talent for evoking deep emotions, I’m captivates audiences, leaving a lasting impact. An artist committed to storytelling,  and continue to push the boundaries of performance.'
+      ]
+    });
+  }
+
+  toggleEditMode() {
+    this.isEditMode = !this.isEditMode;
+  }
+
+  onSubmit() {
+    if (this.profileForm.valid) {
+
+      this.toggleEditMode();
+    }
+  }
+
+
+
 
 }
