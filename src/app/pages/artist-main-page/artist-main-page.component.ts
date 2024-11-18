a9df50fc60e4447516aaced1a033d97474c9bcae
Priya-kamble-154 <priyakamble2109@gmail.com>
2024-11-18 12:35:06 +0530

    changes in Main Artist page are done

--- src/app/pages/artist-main-page/artist-main-page.component.ts
+++ src/app/pages/artist-main-page/artist-main-page.component.ts
@@ -4,14 +4,25 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { SearchHeaderComponent } from '../../shared/search-header/search-header.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-artist-main-page',
   standalone: true,
-  imports: [NavbarComponent,CommonModule,FooterComponent,SearchHeaderComponent],
+  imports: [NavbarComponent,CommonModule,FooterComponent,SearchHeaderComponent,FormsModule],
   templateUrl: './artist-main-page.component.html',
   styleUrl: './artist-main-page.component.css'
 })
 export class ArtistMainPageComponent {
+
+  categories = [
+    { name: 'Artist', checked: false },
+    { name: 'Director', checked: false },
+    { name: 'Casting Director', checked: false },
+    { name: 'Assistant Director', checked: false },
+    { name: 'Associate Director', checked: false },
+    { name: 'Executive Producer', checked: false },
+    { name: 'Production Manager', checked: false }
+  ];
 
 }
