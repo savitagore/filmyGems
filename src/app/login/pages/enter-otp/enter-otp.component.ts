9c34747a5bddce63f1dd7574d900418132fea754
Savitagore <savitag096@gmail.com>
2024-11-18 17:14:54 +0530

    enter-otp

--- src/app/login/pages/enter-otp/enter-otp.component.ts
+++ src/app/login/pages/enter-otp/enter-otp.component.ts
@@ -10,5 +10,26 @@ import { RouterLink } from '@angular/router';
   styleUrl: './enter-otp.component.css'
 })
 export class EnterOTPComponent {
+  otp: string[] = ['', '', '', ''];
 
+  moveToNext(event: any, index: number): void {
+    const input = event.target;
+    if (input.value.length === 1 && index < this.otp.length - 1) {
+      const nextInput = input.nextElementSibling;
+      if (nextInput) {
+        nextInput.focus();
+      }
+    }
+  }
+
+  verifyOTP(): void {
+    const otpValue = this.otp.join('');
+    console.log('Entered OTP:', otpValue);
+    // Add OTP verification logic here
+  }
+
+  resendOTP(): void {
+    console.log('Resend OTP');
+    // Implement OTP resend logic here
+  }
 }
