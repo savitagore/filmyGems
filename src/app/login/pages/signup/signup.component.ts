29242fa115137c37ec0b2f4748cf5bc76541d621
Savitagore <savitag096@gmail.com>
2024-11-18 12:45:17 +0530

    signup-reuseable

--- src/app/login/pages/signup/signup.component.ts
+++ src/app/login/pages/signup/signup.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
+import { LoginCarasoualComponent } from '../../../Reuseable/login-carasoual/login-carasoual.component';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [CommonModule,LoginComponent,RouterLink],
+  imports: [CommonModule,LoginComponent,RouterLink,LoginCarasoualComponent],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
