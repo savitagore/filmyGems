576028998f4d3c278bc9f5bba02dabcf8433c37d
Priya-kamble-154 <priyakamble2109@gmail.com>
2024-12-02 11:02:53 +0530

    reusable validation implemented

--- src/app/login/pages/reset-password/reset-password.component.ts
+++ src/app/login/pages/reset-password/reset-password.component.ts
@@ -3,11 +3,13 @@ import { LoginCarasoualComponent } from '../../../Reuseable/login-carasoual/logi
 import { Router, RouterLink } from '@angular/router';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { SignupvalidationService } from '../../../core/Services/signupValidation/signupvalidation.service';
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
   imports: [LoginCarasoualComponent,RouterLink,CommonModule,ReactiveFormsModule,FormsModule],
+  providers:[SignupvalidationService],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.css'
 })
@@ -15,19 +17,29 @@ export class ResetPasswordComponent {
   resetPasswordForm: FormGroup;
   showPassword1 = false;
   showPassword2 = false;
-  constructor(private router: Router,private fb: FormBuilder){
-    this.resetPasswordForm = this.fb.group({newPassword: [
-      '',
-      [
-        Validators.required,
-        Validators.minLength(8),
-        Validators.pattern(/^(?=.*[A-Z])(?=.*\d).+$/), // At least one uppercase and one number
-      ],
-    ],
-    confirmPassword: ['', [Validators.required]],
-  }, { validators: this.passwordMatchValidator });
-}
+//   constructor(private router: Router,private fb: FormBuilder){
+//     this.resetPasswordForm = this.fb.group({newPassword: [
+//       '',
+//       [
+//         Validators.required,
+//         Validators.minLength(8),
+//         Validators.pattern(/^(?=.*[A-Z])(?=.*\d).+$/), 
+//       ],
+//     ],
+//     confirmPassword: ['', [Validators.required]],
+//   }, { validators: this.passwordMatchValidator });
+// }
  
+
+constructor(private router: Router, private fb: FormBuilder,private signupService:SignupvalidationService) {
+  this.resetPasswordForm = this.fb.group(
+    {
+      password: [ '',[ Validators.required, Validators.minLength(8), Validators.pattern( '^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$' ), ], ],
+      confirmPassword: ['', Validators.required],
+    },
+    { validator: this.matchPasswords }
+  );
+}
   togglePassword(fieldNumber: number) {
     if (fieldNumber === 1) {
       this.showPassword1 = !this.showPassword1;
@@ -35,12 +47,24 @@ export class ResetPasswordComponent {
       this.showPassword2 = !this.showPassword2;
     }
   }
-  
-  passwordMatchValidator(group: FormGroup) {
-    const newPassword = group.get('newPassword')?.value;
+  matchPasswords(group: FormGroup) {
+    const password = group.get('password')?.value;
     const confirmPassword = group.get('confirmPassword')?.value;
-    return newPassword === confirmPassword ? null : { mismatch: true };
+    return password === confirmPassword ? null : { passwordsMismatch: true };
   }
+  passwordMatchValidator(control: FormGroup): { [key: string]: boolean } | null {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+    if (password?.value !== confirmPassword?.value) {
+      return { passwordsMismatch: true };
+    }
+    return null;
+  }
+  // passwordMatchValidator(group: FormGroup) {
+  //   const newPassword = group.get('newPassword')?.value;
+  //   const confirmPassword = group.get('confirmPassword')?.value;
+  //   return newPassword === confirmPassword ? null : { mismatch: true };
+  // }
 
   continueLogin() {
     this.router.navigate(['/continue-login']);
@@ -50,6 +74,19 @@ export class ResetPasswordComponent {
       console.log('Form Invalid');
       this.resetPasswordForm.markAllAsTouched(); // Highlight errors
     }
+  }
+  get password() {
+    return this.resetPasswordForm.get('password');
+  }
+  get confirmPassword() {
+    return this.resetPasswordForm.get('confirmPassword');
+  }
+ 
+  getPasswordPatternTooltip(): string {
+    return this.signupService.getPasswordPatternErrorMessage();
+  }
+  getPasswordMismatchTooltip(): string {
+    return this.signupService.getPasswordMismatchErrorMessage();
   }
 }
 
