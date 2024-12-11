3fd4d319b909fc6f1950854557c76467674e5534
Savitagore <savitag096@gmail.com>
2024-12-11 11:54:02 +0530

    create-profile validations

--- src/app/pages/create-profile/create-profile.component.ts
+++ src/app/pages/create-profile/create-profile.component.ts
@@ -4,51 +4,104 @@ import { NavbarComponent } from '../../shared/navbar/navbar.component';
 
 import { FooterComponent } from '../../shared/footer/footer.component';
 
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ProgressBar, ProgressBarModule } from 'primeng/progressbar';
 
 import { StartjourneyComponent } from '../../shared/startjourney/startjourney.component';
 import { ToastModule } from 'primeng/toast';
-import { MessageService } from 'primeng/api';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { HttpClientModule } from '@angular/common/http';
 import { FileUploadModule } from 'primeng/fileupload';
 import { ButtonModule } from 'primeng/button';
 import { BadgeModule } from 'primeng/badge';
 import { CalendarModule } from 'primeng/calendar';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 
 @Component({
   selector: 'app-create-profile',
   standalone: true,
-  imports: [CommonModule,NavbarComponent,FooterComponent, ReactiveFormsModule, FormsModule, StartjourneyComponent,
-    FileUploadModule, ButtonModule, BadgeModule, ProgressBarModule, ToastModule,CalendarModule],
-  providers: [MessageService],
+  imports: [
+    CommonModule,
+    NavbarComponent,
+    FooterComponent,
+    ReactiveFormsModule,
+    FormsModule,
+    StartjourneyComponent,
+    FileUploadModule,
+    ButtonModule,
+    BadgeModule,
+    ProgressBarModule,
+    ToastModule,
+    CalendarModule,
+    ConfirmDialogModule,
+    ButtonModule,
+  ],
+  providers: [MessageService, ConfirmationService],
   templateUrl: './create-profile.component.html',
-  styleUrl: './create-profile.component.css'
+  styleUrl: './create-profile.component.css',
 })
-export class CreateProfileComponent  {
+export class CreateProfileComponent {
   backgroundImageUrl: string = 'assets/create-profile/bg-main.png';
   bgImage: string = 'assets/create-profile/music-girl.png';
 
   profileForm: FormGroup;
 
-  constructor(private fb: FormBuilder,private messageService: MessageService, private ngZone: NgZone) {
-
+  constructor(
+    private fb: FormBuilder,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
+    private ngZone: NgZone
+  ) {
     this.profileForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern(
+            '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$'
+          ),
+        ],
+      ],
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.minLength(3)]],
       mobile: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
       city: ['', [Validators.required]],
       zip: ['', [Validators.required, Validators.pattern('^[0-9]{6}$')]],
-      country: ['', [Validators.required]]
+      country: ['', [Validators.required]],
     });
   }
-
+  confirm1(event: Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Are you sure that you want to proceed?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      acceptIcon: 'none',
+      rejectIcon: 'none',
+      rejectButtonStyleClass: 'p-button-text',
+      accept: () => {
+        this.messageService.add({
+          severity: 'info',
+          summary: 'Confirmed',
+          detail: 'You have accepted',
+          life: 1000,
+        });
+      },
+      reject: () => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Rejected',
+          detail: 'You have rejected',
+          life: 1000,
+        });
+      },
+    });
+  }
 }
-
-
-
-
-
-
-
